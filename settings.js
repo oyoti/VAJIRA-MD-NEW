2005f574dba3d25afd111124c0a12ecc3d82d6dd
oyoti <155931576+oyoti@users.noreply.github.com>
2024-10-01 19:30:06 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=2ccQARjC#w8mWLJWVA-8ddQUft8yvWOdRNE2WOKIAK55OGhKGkJU' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=LBEUBTBJ#a-rqyrgK0ZGRtM01hZj7Rd7lD7JScWqzfGrBzl7etZ0': process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
